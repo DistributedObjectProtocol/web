eaede461c43a2c3d9e2955d56c2e0cc7b515a266
Josenzo <enzo@josema.es>
2017-01-18 19:11:11 +0800

    Making layout and functionality for slides

--- js/index.js
+++ js/index.js
@@ -1,20 +1,38 @@
-var jsonfile = "resources/slides/snapsvg/main.json",
-    fps = 50,
-    container = "#svg",
-    AJAX_req,
-    slide;
-
-AJAX_req = new XMLHttpRequest();
-AJAX_req.open("GET", jsonfile, true);
+(function() {
+var AJAX_req = new XMLHttpRequest();
+AJAX_req.open("GET", "resources/slides/snapsvg/main.json", true);
 AJAX_req.setRequestHeader("Content-type", "application/json");
-AJAX_req.onreadystatechange = function handle_AJAX_Complete() {
-    if( AJAX_req.readyState == 4 && AJAX_req.status == 200 )
-    {
-        json = JSON.parse(AJAX_req.responseText);
-        comp = new SVGAnim(json, container, fps, null, fps);
-        slide = comp.mc;
-        
-    }
-};
 AJAX_req.send();
+AJAX_req.onreadystatechange = function handle_AJAX_Complete() {
+    if( AJAX_req.readyState == 4 && AJAX_req.status == 200 ) {
 
+        var comp = new SVGAnim(JSON.parse(AJAX_req.responseText), "#svg", null, 50);
+        var slide = comp.mc;
+        var text = "";
+        var textElement = document.getElementById('text');
+        var textVisible = false;
+
+        // We need to delay it, if not SVGAnim will overwrite
+        setTimeout(function(){
+            slide.changeText = function(t) {
+                if (t!==text) {
+                    text = t;
+                    textElement.innerHTML = text;
+                }
+            }
+            slide.showText = function() {
+                if (!textVisible) {
+                    textVisible = true;
+                    textElement.style.opacity = .9;
+                }
+            }
+            slide.hideText = function() {
+                if (textVisible) {
+                    textVisible = false;
+                    textElement.style.opacity = 0;
+                }
+            }
+        }, 1000);
+    }
+}
+})()
