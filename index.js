1ef4668ec6ff9e4bd34b204fbf06a3825d470f49
Josenzo <enzo@josema.es>
2017-01-25 13:09:12 +0800

    Sidebar included dynamically and added edit button on docs

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var path = require('path')
 var fs = require('fs')
 var marked = require('marked')
 var express = require('express')
+var info = require('./info')
 var app = express()
 
 
@@ -15,12 +16,16 @@ app.get('/', function (req, res) {
 
 // docs
 app.get('/:language/:type/:doc', function (req, res) {
+    req.params.language = req.params.language.toLowerCase()
+    req.params.type = req.params.type.toLowerCase()
+    // req.params.doc = req.params.doc.toLowerCase()
     var filepath = path.join(
         __dirname,
         'docs/'+req.params.language+'/'+req.params.type+'/'+req.params.doc+'.md'
     )
     if (fs.existsSync(filepath)) {
         var content = fs.readFileSync(filepath, 'utf8')
+        req.params.info = info[req.params.language]
         req.params.content = marked(content)
         res.render('pages/docs', req.params)
     }
